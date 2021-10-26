ab59c3d1275e16638263dc19f27632d118b27ea0
Nikola <nstipic@etfos.hr>
2021-10-26 17:02:36 +0200

    Zadatak 1.

--- lv1-RUAP/lv1-RUAP/lv1-RUAP.cpp
+++ lv1-RUAP/lv1-RUAP/lv1-RUAP.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od BERNARDE CRNOGORAC (insert index number).\n";
+    std::cout << "Pozdrav iz aplikacije od BERNARDE CRNOGORAC.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
